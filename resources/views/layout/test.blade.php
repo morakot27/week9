334588bec6c015b7a31059b77571bbc1e2d2362a
Morakot <75107121+morakot27@users.noreply.github.com>
2021-06-04 23:59:34 +0700

    task2

---
+++ resources/views/layout/test.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+@include('layout.includes.meta')
+<title>EstateAgency Bootstrap Template - Index</title>
+ @include('layout.includes.favi-css')
+
+  
+</head>
+
+<body>
+
+ 
+@include('layout.includes.nav-tool')
+@include('layout.includes.intro')
+  <main id="main">
+@include('layout.includes.service')
+@include('layout.includes.latestproperty')
+@include('layout.includes.endagentsection')
+@include('layout.includes.latestsec')
+@include('layout.includes.testimo')
+
+    
+
+    
+
+    
+
+    
+
+   
+  </main><!-- End #main -->
+
+  @include('layout.includes.footer')
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('test1');
 });
+
+Route::get('/about', function () {
+    return view('/layout/about');
+});
+
+Route::get('/property', function () {
+    return view('/layout/property');
+});
+Route::get('/blog', function () {
+    return view('/layout/blog');
+});
+
+Route::get('/propertysingle', function () {
+    return view('/layout/propertysingle');
+});
+Route::get('/blogsingle', function () {
+    return view('/layout/blogsingle');
+});
+Route::get('/agentgrid', function () {
+    return view('/layout/agentgrid');
+});
+Route::get('/agentsingle', function () {
+    return view('/layout/agentsingle');
+});
+Route::get('/contact', function () {
+    return view('/layout/contact');
+});
+
+
